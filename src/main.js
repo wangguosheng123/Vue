c6f669b8f709b6173ebcd8e1c03e42fe40ae9f42
DESKTOP-KU6I8BA\admin <hujs@fiberhome.com>
2020-02-08 16:52:51 +0800

    这是利用ant的Vuee项目

---
+++ src/main.js
@@ -0,0 +1,27 @@
+// import Vue from "vue";
+// import App from "./App.vue";
+
+// Vue.config.productionTip = false;
+
+// new Vue({
+//   render: h => h(App)
+// }).$mount("#app");
+
+import Vue from "vue";
+import Antd from "ant-design-vue";
+import App from "./App.vue";
+import "ant-design-vue/dist/antd.css";
+Vue.config.productionTip = false;
+
+Vue.use(Antd);
+
+/* eslint-disable no-new */
+// new Vue({
+//   el: "#app",
+//   components: { App },
+//   template: "<App/>"
+// });
+
+new Vue({
+  render: h => h(App)
+}).$mount("#app");
